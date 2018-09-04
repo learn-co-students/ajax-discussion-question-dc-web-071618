3dc401e1f1e94fee7a1757d0ab0bd4a9f104d9ab
jensnyder <jlsnyder09@gmail.com>
2018-09-04 09:31:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ const fullname = document.getElementById("fullname");
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
+  document.querySelector('button').addEventListener('click', fetchData)
 });
+
+function fetchData() {
+  fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(jsonData => render(jsonData.results[0]))
+}
+
+function render(personData) {
+  fullname.innerText = `${personData.name.first} ${personData.name.last}`
+  email.innerText = personData.email
+  street.innerText = personData.location.street
+  city.innerText = personData.location.city
+  state.innerText = personData.location.state
+  postcode.innerText = personData.location.postcode
+  phone.innerText = personData.phone
+  cell.innerText = personData.cell
+  date_of_birth.innerText = personData.dob.date
+}
